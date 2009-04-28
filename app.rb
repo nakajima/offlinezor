7ca4fdbfb6b1778a182898cdd73c46fc54f83a8d
Pat Nakajima <patnakajima@gmail.com>
2009-04-28 19:28:15 -0400

    Added cache manifest

--- app.rb
+++ app.rb
@@ -5,8 +5,13 @@ class Offlinezor < Sinatra::Default
   set :root, File.dirname(__FILE__)
   set :static, true
   set :public, File.join(root, 'public')
-  
+
   get '/' do
     erb :index
+  end
+
+  get '/manifest' do
+    content_type 'text/cache-manifest'
+    erb :manifest
   end
 end
